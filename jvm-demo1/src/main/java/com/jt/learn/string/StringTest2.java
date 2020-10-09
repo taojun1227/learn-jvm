7236b34847a39b110b20ee6529e8ac4682834a4e
taojun1227 <470804486@qq.com>
2020-10-09 10:22:58 +0800

    字符串底层原理

---
+++ jvm-demo1/src/main/java/com/jt/learn/string/StringTest2.java
@@ -0,0 +1,18 @@
+package com.jt.learn.string;
+
+/**
+ * <p>Title: StringTest2 </p>
+ *
+ * <p>Description:  </p>
+ *
+ * <p>Company: </p>
+ *
+ * <p>date: 2020/9/30 16:39 </p>
+ *
+ * @author: jt-ape
+ */
+public class StringTest2 {
+    public static void main(String[] args) {
+        String s = "你是" + new String("哪个");
+    }
+}
